7f7ac55f9c07edd642a200256c675222d7e10781
hsbauer <bauer.scott@mayo.edu>
2013-11-13 15:50:55 -0600

    dropdown for maps

---
+++ cts2/js/mapDropDown.js
@@ -0,0 +1,24 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: m029206
+ * Date: 11/13/13
+ * Time: 3:34 PM
+ * To change this template use File | Settings | File Templates.
+ */
+var CodeSystemListConfig = {
+    serviceUrl: "http://bmidev4:5555/cts2/mapversions?format=json"
+};
+
+$(document).ready(
+    function() {
+        var url = CodeSystemListConfig.serviceUrl;
+        $.getJSON(url + "&callback=?", function (data) {
+            console.log(url);
+            for (var i in data.mapVersionDirectory.entryList) {
+                var entry = data.mapVersionDirectory.entryList[i];
+                var name = entry.formalName;
+                console.log(name);
+                $("ul.dropdown-menu").append("<li><a href=\"#\">" + name + "</a></li>");
+            }
+        });
+    });
