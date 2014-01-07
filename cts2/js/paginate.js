9a9e117f6f3a8a37e99cf6519b88530bf36adb24
hsbauer <bauer.scott@mayo.edu>
2014-01-07 13:22:26 -0600

    code refactor, created a local jquery reference, fixed a problem with mispaging on a null previous or next url

--- cts2/js/paginate.js
+++ cts2/js/paginate.js
@@ -11,53 +11,38 @@ $(document).ready(function(){
     var url = "http://bmidev4:5555/cts2/codesystem/NCI_Thesaurus/version/10.07e/entities?maxtoreturn=5&format=json&callback=?";
     $.getJSON(url, function(cts2JSON) {
        nextURL = cts2JSON.entityDirectory.next;
-       for(i in cts2JSON.entityDirectory.entryList){
-           var entity = cts2JSON.entityDirectory.entryList[i];
-           var code = entity.name.name;
-           var name = entity.knownEntityDescriptionList[0].designation;
-           if(name == null) {
-               name = "MISSING"
-           }
-
-           $("ul.list-group").append("<li class=\"list-group-item\"><a href=\"#\">code: " + code + "   name: " + name + "</a></li>");
-       }
+       buildHtml(cts2JSON);
     });
 
     $("div.btn-group button.cts2next").on("click", function(){
+        if(nextURL !=null){
         $.when($("div.show_hide").html("<ul class=\"list-group\"></ul>")).then(
-        $.getJSON(nextURL + "&callback=?", function(cts2Next) {
-            nextURL = cts2Next.entityDirectory.next;
-            previousURL = cts2Next.entityDirectory.prev ;
-            for(i in cts2Next.entityDirectory.entryList){
-                var entity = cts2Next.entityDirectory.entryList[i];
-                var code = entity.name.name;
-                var name = entity.knownEntityDescriptionList[0].designation;
-                if(name == null) {
-                    name = "MISSING"
-                }
-
-                $("ul.list-group").append("<li class=\"list-group-item\"><a href=\"#\">code: " + code + "   name: " + name + "</a></li>");
-            }
-        })
-        );
-    })
+            getNextPrev(nextURL))}
+    });
 
     $("div.btn-group button.cts2prev").on("click", function(){
+        if(previousURL != null){
         $.when($("div.show_hide").html("<ul class=\"list-group\"></ul>")).then(
-            $.getJSON(previousURL + "&callback=?", function(cts2Next) {
-                nextURL = cts2Next.entityDirectory.next;
-                previousURL = cts2Next.entityDirectory.prev ;
-                for(i in cts2Next.entityDirectory.entryList){
-                    var entity = cts2Next.entityDirectory.entryList[i];
-                    var code = entity.name.name;
-                    var name = entity.knownEntityDescriptionList[0].designation;
-                    if(name == null) {
-                        name = "MISSING"
-                    }
+        getNextPrev(previousURL)) }
+    });
 
-                    $("ul.list-group").append("<li class=\"list-group-item\"><a href=\"#\">code: " + code + "   name: " + name + "</a></li>");
-                }
-            })
-        );
-    })
+    function buildHtml(data){
+        for(i in data.entityDirectory.entryList){
+            var entity = data.entityDirectory.entryList[i];
+            var code = entity.name.name;
+            var name = entity.knownEntityDescriptionList[0].designation;
+            if(name == null) {
+                name = "MISSING"
+            }
+            $("ul.list-group").append("<li class=\"list-group-item\"><a href=\"#\">code: " + code + "   name: " + name + "</a></li>");
+        }
+    }
+
+    function getNextPrev(givenURL){
+        $.getJSON(givenURL + "&callback=?", function(cts2Next) {
+            nextURL = cts2Next.entityDirectory.next;
+            previousURL = cts2Next.entityDirectory.prev ;
+            buildHtml(cts2Next);
+        })
+    }
 });
